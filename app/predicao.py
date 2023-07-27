b2543079d06ee544dd5e10af29892867cc94293c
mM-b <gtamsue@gmail.com>
2023-07-27 08:58:47 -0300

    models and acuracity

--- app/predicao.py
+++ app/predicao.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 class Predicao:
     def __init__(self):
@@ -15,9 +16,16 @@ class Predicao:
             pass
 
         with col3:
+            st.header("Métricas do modelo")
+            df = pd.read_csv("models/models.csv")
+            col_1, col_2, col_3 = st.columns(3)
+            col_1.metric("Acurácia", value=str(int(100*df['acuracia']))+"%", help="Acurácia do modelo: indica o quão precisa é a previsão de um modelo")
+            col_2.metric("Precisão", value=str(int(100*df['precisao']))+"%", help="Precisão do modelo: indica a capacidade do modelo de prever corretamente os casos em que o paciente tem a doença")
+            col_3.metric("Recall", value=str(int(100*df['recall']))+"%", help="Recall do modelo: indica quantos casos em que o paciente tem a doença o modelo conseguiu identificar no conjunto de dados")
+
+            st.markdown('---')
             st.markdown('<style>p{ text-align: justify;, font-weight: bold;}</style>', unsafe_allow_html=True)
             st.warning("**ATENÇÃO**: Embora os resultados de testes e modelos sejam importantes, é fundamental lembrar que eles não devem ser usados como uma única fonte de informação ou como uma decisão definitiva. É essencial que um profissional de saúde utilize seu conhecimento clínico e julgamento para interpretar e avaliar adequadamente esses resultados, garantindo que os pacientes recebam o tratamento mais adequado e seguro.")
-            
             
 
     def entradas(self):
