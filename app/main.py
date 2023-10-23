597b46a7bfbb1cf8138184319be932897b9ea4af
Rosana <rosanacibely@gmail.com>
2023-10-23 13:38:53 +0000

    fix: fixed path

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class Main():
         st.set_page_config(
                     page_title="Hipotireoidismo",
                     initial_sidebar_state="collapsed",
-                    page_icon='Hypothyroidism/app/icon/cilab.png',
+                    page_icon='app/icon/cilab.png',
                     layout="wide",
                     )
         
@@ -18,7 +18,7 @@ class Main():
         if st.session_state.page == 0:
             self.home()
         if st.session_state.page == 1:
-            hypothyroidism(path="Hypothyroidism/models_file/StackingModel (1).sav")
+            hypothyroidism(path="models_file/StackingModel (1).sav")
             st.button("Voltar",on_click=lambda:self.set_page(0))
 
     def set_page(self,numero):
@@ -31,14 +31,14 @@ class Main():
         st.header("Hipotireoidismo")
         col1, col2 = st.columns(2)
         with col1:
-            st.image('Hypothyroidism/app/icon/icon.png',width=400)
+            st.image('app/icon/icon.png',width=400)
         with col2:
             st.markdown("O hipotireoidismo é uma condição em que a glândula tireoide não produz hormônio tireoidiano suficiente, o que pode resultar em uma diminuição do metabolismo e uma série de sintomas associados. A glândula tireoide está localizada na base do pescoço e é responsável por produzir hormônios que regulam o metabolismo do corpo.")
             st.markdown("O hipotireoidismo primário é a forma mais comum dessa condição, resultando de uma falência da própria glândula tireoide em produzir hormônios suficientes. No entanto, existem outras causas, como o hipotireoidismo central, que ocorre devido a problemas no hipotálamo ou na hipófise, órgãos que desencadeiam a produção hormonal da tireoide. Nesse caso, a origem do distúrbio não está diretamente na glândula tireoide, mas em uma disfunção nos órgãos responsáveis por sua regulação.")
         
         st.header("Sobre diagnósticos")
         st.markdown("O hipotireoidismo é uma disfunção que pode ser diagnóstificada atravez de avaliação clínica, exames físico para procurar sinais de hipotireoidismo, sintomas do paciente, histórico médico. outra maneira de diagnóstico, exames laboratoriais, com base em exames de sangue, observando-se os níveis homonais do TSH, T4, e T3. Auxiliado por um fluxograma.")
-        st.image("Hypothyroidism/app/icon/fluxograma.png",width=900)
+        st.image("app/icon/fluxograma.png",width=900)
         st.markdown("A interpretação dos resultados pode tornar o diagnóstico desafiador para os profissionais de saúde. Com o objetivo de auxiliar nessa questão, um modelo de inteligência artificial foi treinado para prever o diagnóstico do paciente com base em seus níveis hormonais. No entanto, é importante lembrar que a inteligência artificial pode apresentar falsos positivos e falsos negativos; portanto, seu resultado não deve ser considerado absoluto e não deve substituir o julgamento clínico do profissional de saúde. É recomendado que o resultado da inteligência artificial seja interpretado com cautela e que o diagnóstico seja confirmado pelo profissional de saúde.")
         
         st.header("Por que realizar um diagnóstico utilizando inteligência artificial?")
@@ -51,7 +51,7 @@ class Main():
         st.markdown("---")
         st.markdown('<style>h3{font-size: 15px;}</style>', unsafe_allow_html=True)
         st.subheader("Apoio")
-        st.image('Hypothyroidism/app/icon/Ufersa.png', caption='UFERSA', width=70)
+        st.image('app/icon/Ufersa.png', caption='UFERSA', width=70)
 
 
 if __name__ == '__main__':
